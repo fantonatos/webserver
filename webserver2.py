c8d1136fe39a3a3a1d6c7d061bf17930b2089099
fantonatos <fotis.antonatos7@gmail.com>
2022-10-04 17:46:59 -0400

    Webserver 2 working

---
+++ webserver2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+# import socket module
+from threading import Thread
+from socket import *
+
+# In order to terminate the program
+import sys
+
+
+def handle_client(sock, addr):
+    print('Serving client ', addr)
+
+    try:
+        message = sock.recv(1024).decode()
+
+        filename = message.split()[1]
+
+        f = open(filename[1:])
+
+        # read entire file into outputdata
+        outputdata = f.readlines()
+
+        # Send one HTTP header line into socket
+        sock.send('HTTP/1.1 200 OK\r\n'.encode())
+        sock.send('\r\n'.encode())
+
+        # Send the content of the requested file into socket
+        for i in range(0, len(outputdata)):
+            sock.send(outputdata[i].encode())
+        sock.send("\r\n".encode())
+
+    except IOError:
+        # Send response message for file not found
+        sock.send('HTTP/1.1 404 NOT FOUND\r\n'.encode())
+    
+    # Close client socket
+    sock.close()
+
+    
+
+
+# Prepare a sever socket
+# open socket on port 8080
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket.bind(('', 8080))
+serverSocket.listen(10)
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+
+    connectionSocket, addr = serverSocket.accept()
+
+    # run the accept handler in a new thread lambda
+    Thread(target=handle_client, args=(connectionSocket, addr)).start()
+
+# Close server socket
+serverSocket.close()
+
+# Terminate the program after sending the corresponding data
+sys.exit()
+
